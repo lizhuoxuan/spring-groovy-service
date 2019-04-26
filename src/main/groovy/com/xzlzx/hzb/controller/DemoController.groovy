c71213c6bd5d33f013124ec3a8d9287a9a06a739
lzx <123456789>
2019-04-26 15:19:04 +0800

    1

---
+++ src/main/groovy/com/xzlzx/hzb/controller/DemoController.groovy
@@ -0,0 +1,32 @@
+package com.xzlzx.hzb.controller
+
+import groovy.sql.Sql
+import io.swagger.annotations.ApiOperation
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@ResponseBody
+class DemoController {
+
+    @Autowired
+    Sql db
+
+
+    @GetMapping("/test/{id}")
+    def getDemo(@PathVariable String id) {
+        return "get test" + id
+    }
+
+    @GetMapping("/test/db")
+    def dbDemo() {
+        def a= db.firstRow("select * from t_test")
+        return a
+    }
+
+
+}
